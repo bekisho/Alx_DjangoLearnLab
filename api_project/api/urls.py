c4e531725ca903416be18500986cbacc854b046f
Beka Addisu <bekafite@gmail.com>
2025-07-31 05:23:55 -0700

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,3 +4,9 @@ from .views import BookList
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 ]
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
