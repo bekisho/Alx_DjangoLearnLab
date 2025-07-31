246b17b087124b9eb2524f8f01d8feae2903d6c5
Beka Addisu <bekafite@gmail.com>
2025-07-31 06:10:54 -0700

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -23,5 +23,11 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
 
 
