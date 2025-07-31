5400e52a8437cb04abb6c1e8dfc5add06b3db6fc
Beka Addisu <bekafite@gmail.com>
2025-07-31 05:30:22 -0700

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,7 @@
-from django.urls import path
-from .views import BookList
-from django.urls import path
-
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-]
-from django.urls import path
-from .views import BookList
-
-urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # ✅ connects to api/urls.py
 ]
