9de7cc8d783f504f813f1247d675e70a6978ae1d
Beka Addisu <bekafite@gmail.com>
2025-07-31 05:24:34 -0700

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,4 +8,11 @@ from .serializers import BookSerializer
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
