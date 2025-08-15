d04cb5c63b588619988b11a1da3acdf9af9b604e
Beka Addisu <bekafite@gmail.com>
2025-08-15 06:01:22 -0700

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,6 +15,8 @@ from .views import (
 )
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('profile/', views.profile_view, name='profile'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
